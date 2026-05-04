1701d59c9e822174c940689dac11326661aee3a2
josue191005 <josuebecerrav19@gmail.com>
2026-05-04 09:35:15 -0500

    "Primer commit: Estructura base de Spring Boot y paquetes creados"

---
+++ src/main/java/com/upc/trabajoparcial/TrabajoParcialApplication.java
@@ -0,0 +1,13 @@
+package com.upc.trabajoparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoParcialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoParcialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/trabajoparcial/TrabajoParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.trabajoparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoParcialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
